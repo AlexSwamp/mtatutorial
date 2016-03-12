e8123511f47226279acbd59f5e6c04941a6a4507
AlexSwamp <szilagy.sandor@gmail.com>
2016-03-12 17:52:53 +0100

    Update hud.lua

--- hud.lua
+++ hud.lua
@@ -5,11 +5,11 @@ function renderHud()
 	local armor = getPedArmor(localPlayer)
 	local size_w,size_h = 280,8
 
-	local eletCsikSzazalek = (size_w*(hp/100))
+	local hpBar = (size_w*(hp/100))
 	
 	dxDrawRectangle(x/2, 10, 300, 40,tocolor(0, 0, 0, 150))
 	dxDrawRectangle(x/2+10, 12, size_w, size_h,tocolor(0, 0, 0, 255/100*30))
-	dxDrawRectangle(x/2+10, 12, eletCsikSzazalek, size_h,tocolor(210, 77, 87, 255/100*30))
+	dxDrawRectangle(x/2+10, 12, hpBar, size_h,tocolor(210, 77, 87, 255/100*30))
 
 	dxDrawImage(x/2,10,100,100,'files/ezamegjelenitendokep.png')
 
