14a94092a9f99bee68bae373c97fbf51369c6d8e
AlexSwamp <szilagy.sandor@gmail.com>
2016-03-12 17:57:00 +0100

    Update hud.lua

--- hud.lua
+++ hud.lua
@@ -3,13 +3,12 @@ local x,y = guiGetScreenSize()
 function renderHud()
 	local hp = getElementHealth(localPlayer)
 	local armor = getPedArmor(localPlayer)
-	local size_w,size_h = 280,8
-
-	local hpBar = (size_w*(hp/100))
+	local size_w,size_h = 280,8 -- panel méretei
+	local hpBar = (size_w*(hp/100)) -- hp kiszámítása szélességre
 	
-	dxDrawRectangle(x/2, 10, 300, 40,tocolor(0, 0, 0, 150))
-	dxDrawRectangle(x/2+10, 12, size_w, size_h,tocolor(0, 0, 0, 255/100*30))
-	dxDrawRectangle(x/2+10, 12, hpBar, size_h,tocolor(210, 77, 87, 255/100*30))
+	dxDrawRectangle(x/2, 10, 300, 40,tocolor(0, 0, 0, 150)) -- panel bg
+	dxDrawRectangle(x/2+10, 12, size_w, size_h,tocolor(0, 0, 0, 255/100*30)) -- hp bg
+	dxDrawRectangle(x/2+10, 12, hpBar, size_h,tocolor(210, 77, 87, 255/100*30)) -- hp kirajzolása
 
 	dxDrawImage(x/2,10,100,100,'files/ezamegjelenitendokep.png')
 
