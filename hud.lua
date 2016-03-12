5246d1363543ccd5af9e9f86b0124f71b24b5d0d
AlexSwamp <szilagy.sandor@gmail.com>
2016-03-12 17:21:41 +0100

    Create hud.lua

---
+++ hud.lua
@@ -0,0 +1,18 @@
+local x,y = guiGetScreenSize()
+
+function renderHud()
+	local hp = getElementHealth(localPlayer)
+	local armor = getPedArmor(localPlayer)
+	local size_w,size_h = 280,8
+
+	local eletCsikSzazalek = (size_w*(hp/100))
+	
+	dxDrawRectangle(x/2, 10, 300, 40,tocolor(0, 0, 0, 150))
+	dxDrawRectangle(x/2+10, 12, size_w, size_h,tocolor(0, 0, 0, 255/100*30))
+	dxDrawRectangle(x/2+10, 12, eletCsikSzazalek, size_h,tocolor(210, 77, 87, 255/100*30))
+
+	dxDrawImage(x/2,10,100,100,'files/ezamegjelenitendokep.png')
+
+
+end
+addEventHandler("onClientRender",getRootElement(),renderHud)
